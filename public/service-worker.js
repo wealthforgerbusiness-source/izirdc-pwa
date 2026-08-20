006556397d139b5504ae5bdfd712cfded67bf6a5
wealthforgerbusiness-source <wealthforgerbusiness@gmail.com>
2026-08-20 12:40:33 +0100

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // Pas d'interception pour l'instant : tout passe directement au réseau.
+});
